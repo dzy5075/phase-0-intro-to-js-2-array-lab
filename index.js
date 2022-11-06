a0ad926e7d870163e982d25d36924a84f98a8320
Daniel Yim <daniel.yim96@gmail.com>
2022-11-06 17:32:47 -0500

    commit tree d3640614e7e6191ff371b356cee52bbb19db4104

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+}
+function destructivelyPrependCat(Ralph) {
+    cats.unshift(Ralph);
+}
+function destructivelyRemoveLastCat(Ralph) {
+    cats.pop(Ralph);
+}
+function destructivelyRemoveFirstCat(Ralph) {
+    cats.shift(Ralph);
+}
+
+function appendCat(Ralph){
+    const newCats = [...cats.slice(0,3), Ralph]
+   return newCats;
+}
+function prependCat(name){
+    const newCats = [...cats]
+    newCats.splice(0,0, name)
+    return newCats;
+}
+function removeLastCat() {
+    const newCats = [...cats]
+    newCats.splice(-1,1)
+    return newCats;
+}
+function removeFirstCat() {
+    const newCats = [...cats]
+    newCats.splice(0,1)
+    return newCats;
+}
